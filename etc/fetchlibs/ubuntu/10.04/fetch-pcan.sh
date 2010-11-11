95811941d268dc59a4f44fc9cabab59e122963e5
Daniel Schmidt <d_smith@informatik.uni-kl.de>
2010-11-11 19:19:48 +0000

    switched pcan to newest version and compiled in no netdev mode as character device

--- etc/fetchlibs/ubuntu/10.04/fetch-pcan.sh
+++ etc/fetchlibs/ubuntu/10.04/fetch-pcan.sh
@@ -5,10 +5,12 @@ set -e
 
 mkdir -p temp
 cd temp
-wget http://www.peak-system.com/fileadmin/media/linux/files/peak-linux-driver.6.19.tar.gz
-tar -xzf peak-linux-driver.6.19.tar.gz
-cd peak-linux-driver-6.19
+wget http://www.peak-system.com/fileadmin/media/linux/files/peak-linux-driver.6.20.tar.gz
+tar -xzf peak-linux-driver.6.20.tar.gz
+cd peak-linux-driver-6.20
 sudo apt-get install build-essential libpopt-dev
-make
+make clean
+make NET=NO
+
 sudo make install
 
