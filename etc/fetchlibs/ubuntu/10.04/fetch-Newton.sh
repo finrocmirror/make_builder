0e1c8c6d6d04a64e868d1a59fed82f2ec3ac323c
Max Reichardt <reichardt@informatik.uni-kl.de>
2011-01-14 10:06:50 +0000

    Updated fetch-pcan to download version 6.24. Added fetch-pcan to Ubuntu 10.10 scripts. Modified fetch-Newton to download 64bit version on 64bit platforms.

--- etc/fetchlibs/ubuntu/10.04/fetch-Newton.sh
+++ etc/fetchlibs/ubuntu/10.04/fetch-Newton.sh
@@ -4,8 +4,14 @@ set -o verbose #echo on
 
 mkdir -p temp
 cd temp
-wget http://www.newtondynamics.com/downloads/NewtonLinux-32-2.08.tar.gz
-sudo cat NewtonLinux-32-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
+ARCH=`arch`
+if [ $ARCH == 'x86_64' ]; then
+  wget http://www.newtondynamics.com/downloads/NewtonLinux-64-2.08.tar.gz
+  sudo cat NewtonLinux-64-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
+else
+  wget http://www.newtondynamics.com/downloads/NewtonLinux-32-2.08.tar.gz
+  sudo cat NewtonLinux-32-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
+fi
 sudo ln -f -s /usr/local/newtonSDK/sdk/Newton.h /usr/local/include/Newton.h
 sudo ln -f -s /usr/local/newtonSDK/sdk/libNewton.a /usr/local/lib/libNewton.a
 sudo ln -f -s /usr/local/newtonSDK/sdk/libNewton.so /usr/local/lib/libNewton.so
