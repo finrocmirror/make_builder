98ac5e8c59b6181d74bf679c9eb888a92a3081d4
Max Reichardt <reichardt@informatik.uni-kl.de>
2011-01-14 12:06:18 +0000

    fixed fetch-Newton script

--- etc/fetchlibs/ubuntu/10.04/fetch-Newton.sh
+++ etc/fetchlibs/ubuntu/10.04/fetch-Newton.sh
@@ -7,10 +7,10 @@ cd temp
 ARCH=`arch`
 if [ $ARCH == 'x86_64' ]; then
   wget http://www.newtondynamics.com/downloads/NewtonLinux-64-2.08.tar.gz
-  sudo cat NewtonLinux-64-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
+  cat NewtonLinux-64-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
 else
   wget http://www.newtondynamics.com/downloads/NewtonLinux-32-2.08.tar.gz
-  sudo cat NewtonLinux-32-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
+  cat NewtonLinux-32-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
 fi
 sudo ln -f -s /usr/local/newtonSDK/sdk/Newton.h /usr/local/include/Newton.h
 sudo ln -f -s /usr/local/newtonSDK/sdk/libNewton.a /usr/local/lib/libNewton.a
