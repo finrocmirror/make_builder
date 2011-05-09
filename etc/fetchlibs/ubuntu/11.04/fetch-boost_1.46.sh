9aae5becb5edae9fc5a552b97466ed6955430f6e
Max Reichardt <reichardt@informatik.uni-kl.de>
2011-05-09 17:22:42 +0000

    Added scripts to fetch boost 1.46

---
+++ etc/fetchlibs/ubuntu/11.04/fetch-boost_1.46.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o verbose #echo on
+set -e
+
+sudo apt-get install bjam libbz2-dev
+mkdir -p temp
+cd temp
+wget -O boost_1_46_1.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.46.1/boost_1_46_1.tar.bz2/download
+tar -xjf boost_1_46_1.tar.bz2
+cd boost_1_46_1
+bjam
+sudo bjam install
+sudo ldconfig
+
