a3c4f8abe18946844240dffd13880f7659f18407
Max Reichardt <reichardt@informatik.uni-kl.de>
2011-06-11 23:04:50 +0000

    Updated fetch-pcan script to fetch latest pcan driver which also works with kernel 2.6.38 (as used in Ubuntu 11.04)

--- etc/fetchlibs/ubuntu/10.04/fetch-pcan.sh
+++ etc/fetchlibs/ubuntu/10.04/fetch-pcan.sh
@@ -5,9 +5,9 @@ set -e
 
 mkdir -p temp
 cd temp
-wget http://www.peak-system.com/fileadmin/media/linux/files/peak-linux-driver-6.24.tar.gz
-tar -xzf peak-linux-driver-6.24.tar.gz
-cd peak-linux-driver-6.24
+wget http://www.peak-system.com/fileadmin/media/linux/files/peak-linux-driver-7.2.tar.gz
+tar -xzf peak-linux-driver-7.2.tar.gz
+cd peak-linux-driver-7.2
 sudo apt-get install build-essential libpopt-dev
 make clean
 make NET=NO
