05009f0f3b0205654d3455af0d071cc33d41172b
Tobias Föhst <foehst@informatik.uni-kl.de>
2011-09-12 22:21:23 +0000

    Adapted fetch-script to newer kernels in 11.04

--- etc/fetchlibs/ubuntu/11.04/fetch-boost_1.46.sh
+++ etc/fetchlibs/ubuntu/11.04/fetch-boost_1.46.sh
@@ -3,7 +3,7 @@
 set -o verbose #echo on
 set -e
 
-sudo apt-get install bjam libbz2-dev
+sudo apt-get install bjam libbz2-dev python-dev
 mkdir -p temp
 cd temp
 wget -O boost_1_46_1.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.46.1/boost_1_46_1.tar.bz2/download
