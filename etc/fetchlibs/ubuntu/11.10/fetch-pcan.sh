58133570578457af00db3a24146f1da6be72296e
Max Reichardt <reichardt@informatik.uni-kl.de>
2011-10-14 12:17:08 +0000

    Added meta package and two fetch scripts for Ubuntu 11.10.

---
+++ etc/fetchlibs/ubuntu/11.10/fetch-pcan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o verbose #echo on
+set -e
+
+mkdir -p temp
+cd temp
+wget http://www.peak-system.com/fileadmin/media/linux/files/peak-linux-driver-7.4.tar.gz
+tar -xzf peak-linux-driver-7.4.tar.gz
+cd peak-linux-driver-7.4
+sudo apt-get install build-essential libpopt-dev
+make clean
+make NET=NO
+
+sudo make install
+
