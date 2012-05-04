b31f4b2148418e138a920059d498889c250beb68
Max Reichardt <reichardt@informatik.uni-kl.de>
2012-05-04 23:10:22 +0000

    Added meta package for Ubuntu 12.04.

--- etc/fetchlibs/ubuntu/10.04/fetch-Newton.sh
+++ etc/fetchlibs/ubuntu/10.04/fetch-Newton.sh
@@ -5,14 +5,14 @@ set -o verbose #echo on
 mkdir -p temp
 cd temp
 ARCH=`arch`
-if [ $ARCH == 'x86_64' ]; then
-  wget http://www.newtondynamics.com/downloads/NewtonLinux-64-2.08.tar.gz
-  cat NewtonLinux-64-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
-else
-  wget http://www.newtondynamics.com/downloads/NewtonLinux-32-2.08.tar.gz
-  cat NewtonLinux-32-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
-fi
-sudo ln -f -s /usr/local/newtonSDK/sdk/Newton.h /usr/local/include/Newton.h
-sudo ln -f -s /usr/local/newtonSDK/sdk/libNewton.a /usr/local/lib/libNewton.a
-sudo ln -f -s /usr/local/newtonSDK/sdk/libNewton.so /usr/local/lib/libNewton.so
+wget http://newton-dynamics.googlecode.com/files/newton-dynamics-2.35.rar
+sudo apt-get install unrar
+#if [ $ARCH == 'x86_64' ]; then
+#  cat NewtonLinux-64-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
+#else
+#  cat NewtonLinux-32-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
+#fi
+#sudo ln -f -s /usr/local/newtonSDK/sdk/Newton.h /usr/local/include/Newton.h
+#sudo ln -f -s /usr/local/newtonSDK/sdk/libNewton.a /usr/local/lib/libNewton.a
+#sudo ln -f -s /usr/local/newtonSDK/sdk/libNewton.so /usr/local/lib/libNewton.so
 
