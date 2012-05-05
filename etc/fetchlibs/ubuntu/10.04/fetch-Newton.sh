75042cb1549a1ff0174f72865ef662efe9e06d2c
Max Reichardt <reichardt@informatik.uni-kl.de>
2012-05-05 09:28:30 +0000

    Updated fetch-Newton script.

--- etc/fetchlibs/ubuntu/10.04/fetch-Newton.sh
+++ etc/fetchlibs/ubuntu/10.04/fetch-Newton.sh
@@ -7,12 +7,18 @@ cd temp
 ARCH=`arch`
 wget http://newton-dynamics.googlecode.com/files/newton-dynamics-2.35.rar
 sudo apt-get install unrar
-#if [ $ARCH == 'x86_64' ]; then
-#  cat NewtonLinux-64-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
-#else
-#  cat NewtonLinux-32-2.08.tar.gz | sudo tar -C /usr/local/ -xz 
-#fi
-#sudo ln -f -s /usr/local/newtonSDK/sdk/Newton.h /usr/local/include/Newton.h
-#sudo ln -f -s /usr/local/newtonSDK/sdk/libNewton.a /usr/local/lib/libNewton.a
-#sudo ln -f -s /usr/local/newtonSDK/sdk/libNewton.so /usr/local/lib/libNewton.so
+unrar x newton-dynamics-2.35.rar newton-dynamics-2.35/coreLibrary_200
+mkdir -p newton-dynamics-2.35/packages/linux32
+mkdir -p newton-dynamics-2.35/packages/linux64
+cd newton-dynamics-2.35/coreLibrary_200/projets
+if [ $ARCH == 'x86_64' ]; then
+  cd linux64
+else
+  cd linux32
+fi
+make
+sudo cp libNewton.so /usr/local/lib
+sudo cp libNewton.a /usr/local/lib
+sudo cp ../../source/newton/Newton.h /usr/local/include
+
 
