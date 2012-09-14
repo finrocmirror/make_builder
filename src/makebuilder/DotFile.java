269b04f2bae066d38cbb7df7b454695ead893350
Max Reichardt <mreichardt@gmx.net>
2012-09-14 17:03:12 +0200

    Tidied up dependency graph output. Furthermore, lines of code are added, if sloccount is installed.

--- src/makebuilder/DotFile.java
+++ src/makebuilder/DotFile.java
@@ -27,6 +27,9 @@ import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.List;
 
+import makebuilder.util.Files;
+
+
 /**
  * @author max
  *
@@ -34,14 +37,20 @@ import java.util.List;
  */
 public class DotFile {
 
+    private static final boolean SLOCCOUNT_AVAILABLE = new File("/usr/bin/sloccount").exists();
+
     /**
      * Write dot file
      *
      * @param file File to write to
      * @param buildEntities List of build entities
      */
-    public static void write(File file, List<BuildEntity> buildEntities) {
+    public static void write(File file, List<BuildEntity> buildEntities, SourceScanner scanner) {
         try {
+            for (BuildEntity be : buildEntities) {
+                be.params.put("sloc", "" + getSlocCount(be, scanner));
+            }
+
             System.out.println("Writing dot file");
             PrintStream ps = new PrintStream(new BufferedOutputStream(new FileOutputStream(file)));
             ps.println("digraph makebuilder");
@@ -51,11 +60,13 @@ public class DotFile {
                     continue;
                 }
                 for (BuildEntity dep : be.dependencies) {
-                    ps.println("    \"" + be.name + "\" -> \"" + dep.name + "\";");
+                    if (!hasIndirectDependency(be, dep, be)) {
+                        ps.println("    \"" + createName(be) + "\" -> \"" + createName(dep) + "\";");
+                    }
                 }
                 for (BuildEntity odep : be.optionalDependencies) {
-                    if (!be.dependencies.contains(odep)) {
-                        ps.println("    \"" + be.name + "\" -> \"" + odep.name + "\";");
+                    if (!be.dependencies.contains(odep) && (!hasIndirectDependency(be, odep, be))) {
+                        ps.println("    \"" + createName(be) + "\" -> \"" + createName(odep) + "\";");
                     }
                 }
             }
@@ -66,6 +77,60 @@ public class DotFile {
             System.err.println("Error writing .dot file");
             e.printStackTrace();
         }
+    }
+
+    private static String createName(BuildEntity be) {
+        if (SLOCCOUNT_AVAILABLE) {
+            return be.name + "\\n" + be.params.get("sloc") + " SLOC";
+        }
+        return be.name;
+    }
+
+    private static boolean hasIndirectDependency(BuildEntity be, BuildEntity dep, BuildEntity orgBe) {
+        if (be == dep) {
+            return true;
+        }
+        boolean result = false;
+        for (BuildEntity d : be.dependencies) {
+            if (!(be == orgBe && d == dep)) {
+                result |= hasIndirectDependency(d, dep, orgBe);
+            }
+        }
+        for (BuildEntity d : be.optionalDependencies) {
+            if (!(be == orgBe && d == dep)) {
+                result |= hasIndirectDependency(d, dep, orgBe);
+            }
+        }
+        return result;
+    }
+
+    private static String getSlocCount(BuildEntity be, SourceScanner scanner) {
+        String fileList = "";
+        for (SrcFile sf : scanner.getAllFiles()) {
+            if (sf.getOwner() == be) {
+                fileList += " " + sf.relative;
+            }
+        }
+
+        try {
+            Process p = Runtime.getRuntime().exec("sloccount " + fileList);
+            p.waitFor();
+            List<String> lines = Files.readLines(p.getInputStream());
+            boolean start = false;
+            int count = 0;
+            for (String line : lines) {
+                start |= line.equals("Totals grouped by language (dominant language first):");
+                if (start && (line.startsWith("cpp:") || line.startsWith("ansic:"))) {
+                    line = line.substring(line.indexOf(":") + 1);
+                    line = line.substring(0, line.indexOf("(") - 1);
+                    count = Integer.parseInt(line.trim());
+                }
+            }
+            return "" + count;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "error";
     }
 
 }
